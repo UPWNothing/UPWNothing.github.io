66532b9995061c4fb6df7b5ca76dc9d722febdd6
UPWNothing <yc2394@nyu.edu>
2019-01-22 03:59:52 -0500

    Site updated: 2019-01-22 03:59:50

--- js/src/scroll-cookie.js
+++ js/src/scroll-cookie.js
@@ -1,3 +1,3 @@
-// build time:Thu Dec 20 2018 05:43:07 GMT-0500 (Eastern Standard Time)
+// build time:Tue Jan 22 2019 03:59:45 GMT-0500 (Eastern Standard Time)
 $(document).ready(function(){var o=window.location.href.replace(window.location.origin,"");var i;$(window).on("scroll",function(){clearTimeout(i);i=setTimeout(function(){Cookies.set("scroll-cookie",$(window).scrollTop()+"|"+o,{expires:365,path:""})},250)});if(Cookies.get("scroll-cookie")!==undefined){var e=Cookies.get("scroll-cookie").split("|");if(e[1]===o){$(window).scrollTop(e[0])}}});
 //rebuild by neat 
